c65a20f8409a5ce2b3c38dd24381d3d7dbcc007a
Elian Becali <elianbecaliaguiar@gmail.com>
2021-03-10 10:49:22 -0400

    Challenge done: add, delete and change status task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksUpdated = tasks.map(task => {
+      if (task.id !== id) return task;
+
+      return {
+        id: task.id,
+        title: task.title,
+        isComplete: !task.isComplete
+      };
+    });
+
+    setTasks(tasksUpdated);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksUpdated = tasks.filter(task => task.id !== id);
+
+    setTasks(tasksUpdated);
   }
 
   return (
